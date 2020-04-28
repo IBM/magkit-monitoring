2e452cb1218c574f4dcf049aab66156b7ebc7be2
MIHAELA PAPARETE <mihaela.paparete@ro.ibm.com>
2020-04-28 14:12:09 +0300

    MGKT-515: Scheduler Endpoint

---
+++ src/main/java/com/aperto/magkit/monitoring/endpoint/scheduler/SchedulerService.java
@@ -0,0 +1,54 @@
+package com.aperto.magkit.monitoring.endpoint.scheduler;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.quartz.CronExpression;
+
+import info.magnolia.module.scheduler.JobDefinition;
+import info.magnolia.module.scheduler.SchedulerModule;
+
+/**
+ * 
+ * This is the service class for Scheduler endpoint
+ * 
+ * @author MIHAELA PAPARETE (IBM)
+ * @since 2020-04-23
+ *
+ */
+public class SchedulerService {
+	private SchedulerModule schedulerModule;
+
+	@Inject
+	protected SchedulerService(SchedulerModule module) {
+		this.schedulerModule = module;
+	}
+
+	public List<JobInfo> getEnabledJobs() throws Exception {
+		List<JobInfo> scheduledJobs = new ArrayList<JobInfo>();
+
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+		for (JobDefinition job : schedulerModule.getJobs()) {
+			if (job.isEnabled()) {
+				JobInfo jobInfo = new JobInfo();
+
+				String cronExpression = job.getCron();
+				CronExpression cron = new CronExpression(cronExpression);
+				Date nextExecution = cron.getNextValidTimeAfter(new Date());
+
+				jobInfo.setName(job.getName());
+				jobInfo.setDescription(job.getDescription());
+				jobInfo.setCron(cronExpression);
+				jobInfo.setNextExecution(formatter.format(nextExecution));
+				scheduledJobs.add(jobInfo);
+			}
+		}
+
+		return scheduledJobs;
+	}
+}
