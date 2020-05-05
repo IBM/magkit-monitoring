1047d23f4d714ad768445eac235177d5bce91701
MIHAELA PAPARETE <mihaela.paparete@ro.ibm.com>
2020-05-05 18:53:58 +0300

    MGKT-509: Fixed errors raised by Checkstyle rules

--- src/main/java/com/aperto/magkit/monitoring/endpoint/heap/HeapDumpEndpoint.java
+++ src/main/java/com/aperto/magkit/monitoring/endpoint/heap/HeapDumpEndpoint.java
@@ -12,14 +12,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aperto.magkit.monitoring.endpoint.AbstractMonitoringEndpoint;
 import com.aperto.magkit.monitoring.endpoint.MonitoringEndpointDefinition;
-import com.aperto.magkit.monitoring.endpoint.info.InfoEndpoint;
 import com.sun.management.HotSpotDiagnosticMXBean;
 
 import info.magnolia.rest.DynamicPath;
@@ -36,56 +33,46 @@ import info.magnolia.rest.DynamicPath;
 @DynamicPath
 public class HeapDumpEndpoint extends AbstractMonitoringEndpoint<MonitoringEndpointDefinition> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HeapDumpEndpoint.class);
-	
-	public static final Integer LINE_LIMIT 		= 1000;	
-	public static final String FILE_LOCATION 	= "tmp/heapdump";
-	public static final String FILE_NAME 		= "heap_dump";
-	public static final String FILE_EXTENSION   = "-jmap.hprof";
-	public static final String FILE_ENCODING 	= "UTF-8";
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(HeapDumpEndpoint.class);
+
+    public static final Integer LINE_LIMIT = 1000;
+    public static final String FILE_LOCATION = "tmp/heapdump";
+    public static final String FILE_NAME = "heap_dump";
+    public static final String FILE_EXTENSION = "-jmap.hprof";
+    public static final String FILE_ENCODING = "UTF-8";
+
     @Inject
     protected HeapDumpEndpoint(MonitoringEndpointDefinition endpointDefinition) {
         super(endpointDefinition);
     }
 
-	@GET
+    @GET
     @Path("")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response getHeapDump() {
         // by default dump only the live objects
-    	//StringBuffer heapDumpData = new StringBuffer();
-    	File file = null;
+        // StringBuffer heapDumpData = new StringBuffer();
+        File file = null;
         boolean live = true;
         try {
-        	File dir = new File(FILE_LOCATION);
-        	dir.mkdirs();
-        	file = new File(dir, FILE_NAME + System.currentTimeMillis() + FILE_EXTENSION);
-        	String currentFileName = file.getPath();
-			dumpHeap(currentFileName, live);
-//			LineIterator it = FileUtils.lineIterator(file, FILE_ENCODING);
-//			int counter = 0;
-//			try {
-//			    while (it.hasNext() && counter <= LINE_LIMIT) {
-//			        heapDumpData.append(it.nextLine());
-//			        counter++;
-//			    }
-//			} finally {
-//			    LineIterator.closeQuietly(it);
-//			}
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage(), e);
-		}
-        //return heapDumpData.toString();
+            File dir = new File(FILE_LOCATION);
+            dir.mkdirs();
+            file = new File(dir, FILE_NAME + System.currentTimeMillis() + FILE_EXTENSION);
+            String currentFileName = file.getPath();
+            dumpHeap(currentFileName, live);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+
+        // Header setting is optional
         return Response.ok(file, MediaType.APPLICATION_OCTET_STREAM)
-        	      .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"" ) //optional
-        	      .build();
+                .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"").build();
     }
-    
+
     public static void dumpHeap(String filePath, boolean live) throws IOException {
         MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-        HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(
-          server, "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+        HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(server,
+                "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
         mxBean.dumpHeap(filePath, live);
     }
 
