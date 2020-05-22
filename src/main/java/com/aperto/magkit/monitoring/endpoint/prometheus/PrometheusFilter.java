9b7a97893de46b37b9166124356dfc98800fd8ed
vlad.nacu <Vlad.Nacu@ibm.com>
2020-05-22 13:15:15 +0300

    MGKT-516: Fixed Prometheus filter

--- src/main/java/com/aperto/magkit/monitoring/endpoint/prometheus/PrometheusFilter.java
+++ src/main/java/com/aperto/magkit/monitoring/endpoint/prometheus/PrometheusFilter.java
@@ -22,28 +22,55 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 public class PrometheusFilter extends AbstractMgnlFilter {
 
     private final PrometheusMeterRegistry _registry;
-    
+
     @Inject
     public PrometheusFilter(PrometheusMeterRegistry registry) {
         _registry = registry;
-        
+
     }
-    
+
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        
+
         String httpPath = StringUtils.isNotEmpty(request.getRequestURI()) ? request.getRequestURI() : "";
         String httpMethod = StringUtils.isNotEmpty(request.getMethod()) ? request.getMethod() : "";
         String httpStatus = String.valueOf(response.getStatus());
-        
+
         Counter httpRequestsTotal = Counter.builder("http_requests_count")
                 .description("A counter of the total number of HTTP requests.")
-                .tags("method", request.getMethod(), "status", httpStatus, "uri", httpPath)
-                .register(_registry);
+                .tags("method", request.getMethod(), "status", httpStatus, "uri", httpPath).register(_registry);
+
+        /*
+         * DistributionSummary histogram =
+         * DistributionSummary.builder("http_requests_duration_bucket") .tags("method",
+         * request.getMethod(), "status", httpStatus, "uri",
+         * httpPath).publishPercentileHistogram() .register(_registry);
+         */
+
+        /*
+         * DistributionSummary histogramSla =
+         * DistributionSummary.builder("http_requests_duration_bucket_ms")
+         * .tags("method", request.getMethod(), "status", httpStatus, "uri", httpPath,
+         * "le", "10") .sla(100, 500, 1000,
+         * 10000).publishPercentileHistogram().register(_registry);
+         */
+
+        /*
+         * DistributionSummary.builder("my.ratio").scale(100).sla(70, 80,
+         * 90).register(_registry);
+         */
+
+        /*
+         * Timer myTimer = (Timer) Timer.builder("my.timer").publishPercentiles(0.5,
+         * 0.95).publishPercentileHistogram()
+         * .sla(Duration.ofMillis(100)).minimumExpectedValue(Duration.ofMillis(1))
+         * .maximumExpectedValue(Duration.ofSeconds(10)).register(_registry);
+         */
 
         httpRequestsTotal.increment();
-    }
+        chain.doFilter(request, response);
 
+    }
 
 }
