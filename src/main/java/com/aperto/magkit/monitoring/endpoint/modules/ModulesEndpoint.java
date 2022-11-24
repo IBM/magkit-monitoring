803044da999bfb21c5ee598b967794b3b2fba8b4
soenke.schmidt <soenke.schmidt@ibmix.de>
2022-11-24 10:31:07 +0100

    NO-TICKET:  Fixed Modules Endpoint to properly provide light module information

--- src/main/java/com/aperto/magkit/monitoring/endpoint/modules/ModulesEndpoint.java
+++ src/main/java/com/aperto/magkit/monitoring/endpoint/modules/ModulesEndpoint.java
@@ -1,23 +1,24 @@
 package com.aperto.magkit.monitoring.endpoint.modules;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
-import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.aperto.magkit.monitoring.endpoint.AbstractMonitoringEndpoint;
 import com.aperto.magkit.monitoring.endpoint.MonitoringEndpointDefinition;
 
-import info.magnolia.context.MgnlContext;
-import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.module.ModuleManagementException;
+import info.magnolia.module.ModuleRegistry;
+import info.magnolia.module.model.ModuleDefinition;
+import info.magnolia.module.model.reader.LightModuleDefinitionReader;
 import info.magnolia.rest.DynamicPath;
 
 /**
@@ -33,32 +34,51 @@ import info.magnolia.rest.DynamicPath;
 @DynamicPath
 public class ModulesEndpoint extends AbstractMonitoringEndpoint<MonitoringEndpointDefinition> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ModulesEndpoint.class);
+    private ModuleRegistry _moduleRegistry;
+    private LightModuleDefinitionReader _lightModuleDefinitionReader;
 
     @Inject
-    protected ModulesEndpoint(MonitoringEndpointDefinition endpointDefinition) {
+    protected ModulesEndpoint(MonitoringEndpointDefinition endpointDefinition, ModuleRegistry moduleRegistry, LightModuleDefinitionReader lightModuleDefinitionReader) {
         super(endpointDefinition);
+        _moduleRegistry = moduleRegistry;
+        _lightModuleDefinitionReader = lightModuleDefinitionReader;
     }
 
     @GET
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    public Modules modules() {
+    public List<ModuleResponsePojo> modules() throws ModuleManagementException {
 
-        Modules registeredModules = new Modules();
+        Map<String, ModuleDefinition> lightModuleDefinitonMap = _lightModuleDefinitionReader.readAll();
+        Map<String, ModuleDefinition> registeredModuleDefinitionMap = _moduleRegistry.getModuleDefinitions().stream()
+                .collect(Collectors.toMap(ModuleDefinition::getName, Function.identity()));
 
-        try {
-            Session session = MgnlContext.getJCRSession("config");
-            Node modulesNode = session.getNode("/modules");
-            NodeIterator modulesChildrenIterator = modulesNode.getNodes();
-            Iterable<Node> childrenOfModules = NodeUtil.asIterable(modulesChildrenIterator);
-            childrenOfModules.forEach(c -> registeredModules.addRegisteredModule(c));
+        registeredModuleDefinitionMap.putAll(lightModuleDefinitonMap);
 
-        } catch (RepositoryException e) {
-            LOGGER.error("An error occurred:", e);
+        return registeredModuleDefinitionMap.values().stream()
+            .map((md) -> new ModuleResponsePojo(md.getName(), md.getVersion().toString()))
+            .sorted(Comparator.comparing(ModuleResponsePojo::getName))
+            .collect(Collectors.toList());
+    }
+
+    private static class ModuleResponsePojo {
+
+        private final String _name;
+        private final String _version;
+
+        ModuleResponsePojo(String name, String version) {
+            _name = name;
+            _version = version;
         }
 
-        return registeredModules;
+        public String getName() {
+            return _name;
+        }
+
+        public String getVersion() {
+            return _version;
+        }
+
     }
 
 }
